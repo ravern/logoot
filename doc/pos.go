433dabb4fbd57050dccfec98984e1024b6d3cd1f
Ravern Koh <ravern.koh.dev@gmail.com>
2017-11-15 00:20:30 +0800

    Finished compare and generate Pos

--- doc/pos.go
+++ doc/pos.go
@@ -1,7 +1,10 @@
 package doc
 
+import "math/rand"
+
 // Pos is an element of a position identifier. A position identifier identifies an
-// atom within a Doc.
+// atom within a Doc. The behaviour of an empty position identifier (length == 0) is
+// undefined, so just do not pass in empty position identifiers to any method/function.
 type Pos struct {
 	Ident uint16
 	Site  uint8
@@ -33,7 +36,51 @@ func ComparePos(lp []Pos, rp []Pos) int8 {
 	return 0
 }
 
+// random number between x and y, where y > x.
+func random(x, y uint16) uint16 {
+	return uint16(rand.Intn(int(y-x-1))) + 1 + x
+}
+
 // GeneratePos generates a new position identifier between the two positions provided.
-func GeneratePos(lp []Pos, rp []Pos, site uint8) []Pos {
-	return nil
+// Secondary return value indicates whether it was successful (when the two positions
+// are equal, or the left is greater than right, position cannot be generated).
+func GeneratePos(lp, rp []Pos, site uint8) ([]Pos, bool) {
+	if ComparePos(lp, rp) != -1 {
+		return nil, false
+	}
+	p := []Pos{}
+	for i := 0; i < len(lp); i++ {
+		l := lp[i]
+		r := rp[i]
+		if l.Ident == r.Ident && l.Site == r.Site {
+			p = append(p, Pos{l.Ident, l.Site})
+			continue
+		}
+		if d := r.Ident - l.Ident; d > 1 {
+			r := random(l.Ident, r.Ident)
+			p = append(p, Pos{r, site})
+		} else if d == 1 {
+			if site > l.Site {
+				p = append(p, Pos{l.Ident, site})
+			} else if site < r.Site {
+				p = append(p, Pos{r.Ident, site})
+			} else {
+				r := random(0, ^uint16(0))
+				p = append(p, Pos{l.Ident, l.Site}, Pos{r, site})
+			}
+		} else {
+			if site > l.Site && site < r.Site {
+				p = append(p, Pos{l.Ident, site})
+			} else {
+				r := random(0, ^uint16(0))
+				p = append(p, Pos{l.Ident, l.Site}, Pos{r, site})
+			}
+		}
+		return p, true
+	}
+	if len(rp) > len(lp) {
+		r := random(0, rp[len(lp)].Ident)
+		p = append(p, Pos{r, site})
+	}
+	return p, true
 }
--- doc/pos_test.go
+++ doc/pos_test.go
@@ -1,12 +1,13 @@
 package doc_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/ravernkoh/logoot/doc"
 )
 
-func TestPosCompare(t *testing.T) {
+func TestComparePos(t *testing.T) {
 	tests := []struct {
 		lpos   []doc.Pos
 		rpos   []doc.Pos
@@ -41,8 +42,62 @@ func TestPosCompare(t *testing.T) {
 
 	for i, test := range tests {
 		res := doc.ComparePos(test.lpos, test.rpos)
+
 		if res != test.result {
 			t.Errorf("Test %d: Expected %d, got %d.", i+1, test.result, res)
+		}
+	}
+}
+
+func TestGeneratePos(t *testing.T) {
+	tests := []struct {
+		lpos    []doc.Pos
+		rpos    []doc.Pos
+		success bool
+	}{
+		{
+			[]doc.Pos{{0, 0}},
+			[]doc.Pos{{1, 1}},
+			true,
+		},
+		{
+			[]doc.Pos{{1, 1}},
+			[]doc.Pos{{1, 1}, {3, 2}},
+			true,
+		},
+		{
+			[]doc.Pos{{5, 3}, {11, 10}},
+			[]doc.Pos{{5, 3}, {11, 10}},
+			false,
+		},
+		{
+			[]doc.Pos{{1, 1}, {5, 4}},
+			[]doc.Pos{{1, 1}, {3, 2}},
+			false,
+		},
+		{
+			[]doc.Pos{{1, 1}, {5, 4}},
+			[]doc.Pos{{1, 1}, {5, 4}, {3, 2}},
+			true,
+		},
+		{
+			[]doc.Pos{{0, 1}, {3, 4}},
+			[]doc.Pos{{1, 1}},
+			true,
+		},
+	}
+
+	for i, test := range tests {
+		p, success := doc.GeneratePos(test.lpos, test.rpos, 1)
+
+		l := doc.ComparePos(test.lpos, p)
+		r := doc.ComparePos(p, test.rpos)
+		if success && (l != -1 || r != -1) {
+			fmt.Printf("Test %d, Pos %v\n", i+1, p)
+			t.Errorf("Test %d: Expected generated Pos to be between given Pos(s).", i+1)
+		}
+		if success != test.success {
+			t.Errorf("Test %d: Expected success flag to be %t, but got %t", i+1, test.success, success)
 		}
 	}
 }
