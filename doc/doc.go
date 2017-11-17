7c63e71ac8eb29d500c0a51c9ee07eca4c23f4bb
Ravern Koh <ravern.koh.dev@gmail.com>
2017-11-17 11:33:09 +0800

    Got inserting left and right to work

--- doc/doc.go
+++ doc/doc.go
@@ -1,5 +1,7 @@
 package doc
 
+import "bytes"
+
 // Doc represents a Logoot document. Actions like Insert and Delete can be performed
 // on Doc. If at any time an invalid position is given, a panic will occur, so raw
 // positions should only be used for debugging purposes.
@@ -24,11 +26,11 @@ var (
 func New(content []string, site uint8) *Doc {
 	d := &Doc{site: site}
 	d.Insert(Start, "")
+	d.Insert(End, "")
 	for _, c := range content {
 		// End will always exist.
 		d.InsertLeft(End, c)
 	}
-	d.Insert(End, "")
 	return d
 }
 
@@ -84,26 +86,33 @@ func (d *Doc) Delete(p []Pos) bool {
 
 // Left returns the position to the left of the given position, and a flag indicating
 // whether it exists (when the given position is the start, there is no position to the
-// left of it).
+// left of it). Will be false if the given position is invalid. The Start pair is not
+// considered as an actual pair.
 func (d *Doc) Left(p []Pos) ([]Pos, bool) {
-	return nil, false
-}
-
-// GeneratePos generates a new position identifier between the two positions provided.
-func (d *Doc) GeneratePos(lp []Pos, rp []Pos) []Pos {
-	return nil
+	i, exists := d.Index(p)
+	if !exists || i == 0 {
+		return nil, false
+	}
+	return d.pairs[i-1].pos, true
 }
 
 // Right returns the position to the right of the given position, and a flag indicating
 // whether it exists (when the given position is the end, there is no position to the
-// right of it).
+// right of it). Will be false if the given position is invalid. The End pair is not
+// considered as an actual pair.
 func (d *Doc) Right(p []Pos) ([]Pos, bool) {
-	return nil, false
+	i, exists := d.Index(p)
+	if !exists || i >= len(d.pairs)-1 {
+		return nil, false
+	}
+	return d.pairs[i+1].pos, true
 }
 
-// Each element, along with their positions will be passed to the provided closure
-// in order. The closure should return true to continue or false to break.
-func (d *Doc) Each(f func([]Pos, string) bool) {
+// GeneratePos generates a new position identifier between the two positions provided.
+// Secondary return value indicates whether it was successful (when the two positions
+// are equal, or the left is greater than right, position cannot be generated).
+func (d *Doc) GeneratePos(lp []Pos, rp []Pos) ([]Pos, bool) {
+	return GeneratePos(lp, rp, d.site)
 }
 
 /* Convenience methods */
@@ -112,14 +121,32 @@ func (d *Doc) Each(f func([]Pos, string) bool) {
 // is successful (when the given position doesn't exist, InsertLeft won't do anything
 // and return false).
 func (d *Doc) InsertLeft(p []Pos, atom string) bool {
-	return false
+	lp, success := d.Left(p)
+	if !success {
+		return false
+	}
+	np, success := d.GeneratePos(lp, p)
+	if !success {
+		return false
+	}
+	d.Insert(np, atom)
+	return true
 }
 
 // InsertRight inserts the atom to the right of the given position, returning whether it
 // is successful (when the given position doesn't exist, InsertRight won't do anything
 // and return false).
 func (d *Doc) InsertRight(p []Pos, atom string) bool {
-	return false
+	rp, success := d.Right(p)
+	if !success {
+		return false
+	}
+	np, success := d.GeneratePos(p, rp)
+	if !success {
+		return false
+	}
+	d.Insert(np, atom)
+	return true
 }
 
 // DeleteLeft deletes the atom to the left of the given position, returning whether it
@@ -136,7 +163,11 @@ func (d *Doc) DeleteRight(p []Pos, atom string) bool {
 	return false
 }
 
-// Content of the entire document, except the first and last empty lines.
-func (d *Doc) Content() []string {
-	return nil
+// Content of the entire document.
+func (d *Doc) Content() string {
+	var b bytes.Buffer
+	for i := 1; i < len(d.pairs)-1; i++ {
+		b.WriteString(d.pairs[i].atom)
+	}
+	return b.String()
 }
--- doc/doc_test.go
+++ doc/doc_test.go
@@ -35,3 +35,98 @@ func TestDocGetPos(t *testing.T) {
 		}
 	}
 }
+
+func TestDocInsertAndContent(t *testing.T) {
+	tests := []struct {
+		poss    [][]doc.Pos
+		atoms   []string
+		content string
+	}{
+		{
+			[][]doc.Pos{
+				[]doc.Pos{{1, 1}},
+				[]doc.Pos{{3, 1}},
+				[]doc.Pos{{4, 1}},
+				[]doc.Pos{{6, 1}},
+			},
+			[]string{
+				"hel",
+				"lo ",
+				"wor",
+				"ld",
+			},
+			"hello world",
+		},
+		{
+			[][]doc.Pos{
+				[]doc.Pos{{1, 1}},
+				[]doc.Pos{{1, 1}, {5, 1}},
+				[]doc.Pos{{1, 1}, {5, 1}, {1, 1}},
+				[]doc.Pos{{2, 1}},
+			},
+			[]string{
+				"boo ",
+				"lmao ",
+				"test ",
+				"now",
+			},
+			"boo lmao test now",
+		},
+	}
+
+	for i, test := range tests {
+		d := doc.New([]string{}, 1)
+		for i, pos := range test.poss {
+			d.Insert(pos, test.atoms[i])
+		}
+
+		c := d.Content()
+		if c != test.content {
+			t.Errorf("Test %d: Expected %s, got %s.", i+1, test.content, c)
+		}
+	}
+}
+
+func TestDocLeft(t *testing.T) {
+	tests := []string{
+		"hel",
+		"lo ",
+		"wor",
+		"ld.",
+	}
+
+	d := doc.New(tests, 1)
+	p := doc.End
+	for i := 0; i < len(tests); i++ {
+		newp, exists := d.Left(p)
+		if !exists && i != len(tests)-1 {
+			t.Errorf("Test %d: Expected position to exist.", i+1)
+		}
+		if doc.ComparePos(newp, p) != -1 {
+			t.Errorf("Test %d: Expected new position to be less than old position.", i+1)
+		}
+		p = newp
+	}
+}
+
+func TestDocRight(t *testing.T) {
+	tests := []string{
+		"tes",
+		"tin",
+		"g 1",
+		"23.",
+	}
+
+	d := doc.New(tests, 1)
+	p := doc.Start
+	for i := 0; i < len(tests); i++ {
+		newp, exists := d.Right(p)
+		if !exists && i != len(tests)-1 {
+			t.Errorf("Test %d: Expected position to exist.", i+1)
+		}
+		if doc.ComparePos(newp, p) != 1 {
+			t.Errorf("Test %d: Expected new position to be greater than old position.", i+1)
+		}
+		p = newp
+	}
+}
