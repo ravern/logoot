18fb06354279fcb01479fd775e466e9d768f9271
Ravern Koh <ravern.koh.dev@gmail.com>
2017-11-20 16:27:42 +0800

    Got the basic keyboard to work

--- demo/client/main.go
+++ demo/client/main.go
@@ -5,13 +5,15 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"time"
+	"strings"
 
 	termbox "github.com/nsf/termbox-go"
+	"github.com/ravernkoh/logoot/doc"
 )
 
 var (
 	site uint8
+	ldoc *doc.Doc // Logoot document
 )
 
 func main() {
@@ -35,32 +37,82 @@ func main() {
 	site = uint8([]byte(s)[0])
 	fmt.Printf("Assigned site %d.\n", site)
 
-	err = termbox.Init()
+	c := strings.Split("Hello world", "")
+	ldoc = doc.New(c, site)
+
+	ui(conn)
+	go read(conn)
+
+	fmt.Println("Disconnected!")
+}
+
+func ui(conn net.Conn) {
+	err := termbox.Init()
 	if err != nil {
 		panic(err)
 	}
 	defer termbox.Close()
 
-	// TODO tmp
-	go func() {
-		for {
-			time.Sleep(time.Second)
-			_, err := conn.Write([]byte{site, 0})
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
+	w, h := termbox.Size()
+	p := doc.End
+
+	for {
+		i, _ := ldoc.Index(p)
+		i--
+		termbox.SetCursor(i%w, i/w)
+		r := []rune(ldoc.Content())
+		for x := 0; x < w; x++ {
+			for y := 0; y < h; y++ {
+				j := x + y*w
+				if j < len(r) {
+					termbox.SetCell(x, y, r[j], termbox.ColorWhite, termbox.ColorDefault)
+				}
 			}
 		}
-	}()
+		termbox.Flush()
 
-	// Read
+		e := termbox.PollEvent()
+		switch e.Key {
+		case termbox.KeyCtrlC:
+			os.Exit(0)
+
+		case termbox.KeyArrowUp:
+			if i-w > 0 {
+				for j := 0; j < w; j++ {
+					p, _ = ldoc.Left(p)
+				}
+			}
+		case termbox.KeyArrowDown:
+			if i+w < len(r) {
+				for j := 0; j < w; j++ {
+					p, _ = ldoc.Right(p)
+				}
+			}
+		case termbox.KeyArrowLeft:
+			if i > 0 {
+				p, _ = ldoc.Left(p)
+			}
+		case termbox.KeyArrowRight:
+			if i < len(r) {
+				p, _ = ldoc.Right(p)
+			}
+
+		default:
+			p, _ = ldoc.InsertLeft(p, string(e.Ch))
+			p, _ = ldoc.Right(p)
+		}
+	}
+}
+
+func read(conn net.Conn) {
+	// Create main reader
+	r := bufio.NewReader(conn)
+
 	for {
-		s, err = r.ReadString(0)
+		s, err := r.ReadString(0)
 		if err != nil {
 			panic(err)
 		}
 		fmt.Println([]byte(s)[0])
 	}
-
-	fmt.Println("Disconnected!")
 }
